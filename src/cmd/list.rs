e070863da5ce1a5ce5f519af18f15effb6a38df3
Niels Mentink <niels@ment.ink>
2023-08-21 20:41:17 +0200

    ✨ Added basic pnpm outdated

--- src/cmd/list.rs
+++ src/cmd/list.rs
@@ -1,12 +1,35 @@
-use crate::updater::{pnpm::Pnpm, Updater};
+use std::collections::HashMap;
+
+use crate::updater::{pnpm::Pnpm, Package, Updater};
 
 pub fn run() {
-    let pnpm = Pnpm::new("pnpm");
+    let updaters = vec![Pnpm::new("pnpm")];
 
-    match pnpm.list() {
-        Some(pkgs) => pkgs
-            .iter()
-            .for_each(|pkg| println!("{}: {}", pkg.name, pkg.current_version)),
-        None => panic!("No packages found"),
+    let updates = updaters.iter().fold(
+        HashMap::<String, Vec<Package>>::new(),
+        |mut acc, updater| {
+            if let Some(pkgs) = updater.list() {
+                acc.insert(updater.name(), pkgs);
+            }
+
+            return acc;
+        },
+    );
+
+    for (package_manager, packages) in updates {
+        let (a, b) = package_manager.split_at(1);
+
+        println!(
+            "{} has {} outdated packages",
+            a.to_uppercase() + b,
+            packages.len()
+        );
+
+        for pkg in packages {
+            println!(
+                "{}: {} -> {}",
+                pkg.name, pkg.current_version, pkg.latest_version
+            );
+        }
     }
 }
--- src/updater/pnpm.rs
+++ src/updater/pnpm.rs
@@ -1,3 +1,7 @@
+use std::collections::HashMap;
+
+use serde::{Deserialize, Serialize};
+use serde_json::from_str;
 use subprocess::Exec;
 
 use crate::updater::Package;
@@ -7,6 +11,16 @@ use super::Updater;
 #[derive(Debug)]
 pub struct Pnpm {
     name: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+struct PnpmPackage {
+    current: String,
+    latest: String,
+    wanted: String,
+    is_deprecated: bool,
+    dependency_type: String,
 }
 
 impl Updater for Pnpm {
@@ -21,23 +35,27 @@ impl Updater for Pnpm {
     }
 
     fn list(&self) -> Option<Vec<super::Package>> {
-        let output = Exec::shell(format!("{} outdated --global --json", self.name))
+        let output = match Exec::shell(format!("{} outdated --global --json", self.name))
             .capture()
-            .map(|cmd| cmd.stdout_str());
-
-        return match output {
-            Ok(out) => {
-                let name = out.replace("\n", "");
-                Some(vec![Package {
-                    name,
-                    current_version: "lol".to_owned(),
-                    latest_version: "lol".to_string(),
-                }])
-            }
-            Err(err) => {
-                println!("Error: {}", err);
-                None
-            }
+            .map(|cmd| cmd.stdout_str())
+        {
+            Ok(output) => output,
+            _ => return None,
         };
+
+        match from_str::<HashMap<String, PnpmPackage>>(&output) {
+            Ok(pkgs) => Some(
+                pkgs.iter()
+                    .map(|(name, pkg)| -> Package {
+                        Package {
+                            name: name.to_string(),
+                            current_version: pkg.current.to_string(),
+                            latest_version: pkg.latest.to_string(),
+                        }
+                    })
+                    .collect(),
+            ),
+            _ => None,
+        }
     }
 }
